b162b556bfccd7c123f59be1710089bca813f063
ss-stewart <ss_stewart92@yahoo.com>
2023-04-27 13:31:54 -0400

    add functions and edit DOM look

--- memory-game.js
+++ memory-game.js
@@ -1,45 +1,47 @@
-// games starts when play is clicked
-const frontOfCard = document.getElementsByClassName("frontOfCard")
-const card = document.createElement('p')
-const timer = document.querySelector('#timer')
-const instructionMsg = document.getElementById("msg")
+// CACHED VARIABLES
+
+const frontOfCard = document.getElementsByClassName("frontOfCard");
+const card = document.createElement('p');
+const timer = document.querySelector('#timer');
+const instructionMsg = document.getElementById("msg");
 const startButton = document.getElementById('startGameBtn');
+const restartButton = document.getElementById('playAgainBtn');
+const checkAnsbutton = document.getElementById('checkPlayAnsBtn');
 
 // EVENT LISTENERS
 startButton.addEventListener('click', handleClick);
+restartButton.addEventListener('click', handleClick);
+checkAnsbutton.addEventListener('click', handleClick);
 
 // how to change instruction msg (if ... write the following code)
 // instructionMessage.innerHTML = "new message"
 
-
-init ()
+// Game starts when player hits play button
+    // display instructions for player as a message
+init ();
 function init () {
-    instructionMsg.style.display = "none"
+    instructionMsg.style.display = "none";
 }
+    // get 5 random numbers
     function generateNums () {
-        instructionMsg.style.display = "block"
-        frontOfCard.innerHTML = ''
+        instructionMsg.style.display = "block";
+        frontOfCard.innerHTML = '';
         // Create an empty array for 5 items
-        var fiveRandInt = []
-        // Start a loop and run it 5 times
-        
+        var fiveRandInt = [];
+        // Start a loop and run it 5 times        
         for(i = 0; i < 5; i++) {
-            fiveRandInt[i] = Math.floor( Math.random() * 999 )
-            //card.textContent = fiveRandInt
-            //frontOfCard.appendChild(card)
+            fiveRandInt[i] = Math.floor( Math.random() * 999 );
             }
-            console.log(fiveRandInt)
+            print(fiveRandInt);
     }
-  
-    // Use the index of the loop to assign the random number to that index of the array
 
-        // flip card
+    // flip card[Div] to display fiveRandInt
 
         function handleClick() {
-            // Start the timer
+    // Start the timer; when timer ends, alert player
             let seconds = 10;
             let intervalId = setInterval(function() {
-                timer.innerHTML = `Timer: ${seconds}s`
+                timer.innerHTML = `Timer: ${seconds}s`;
                 seconds--;
               if (seconds < 0) {
                 clearInterval(intervalId);
@@ -49,33 +51,31 @@ function init () {
             
             generateNums();
           }    
-    // function startTimer () {
-    //     timer.innerHTML = ''
+        // flip to back of card - blank  
 
-    //     let i = 10
+// display instructions (enter the numbers) for player as a message
+    // "Enter the numbers you remember in the order that you saw them xx, xx, xx, xx, xx"
+// accept user input
+function acceptPlayerInput() {
+    const PlayerInput = document.querySelector('playerAnswer');
+    const answer = playerInput.numbers;
+  }
 
-    //     while (i >= 0) {
-    //         const para = document.createElement('p')
-    //         if (i === 10) {
-    //             para.textContent = `Countdown ${i}`
-    //         } else if (i<10 && i>0) {
-    //             para.textContent = i
-    //         } else {
-    //             para.textContent = "Time's up!"
-    //         }
-    //         timer.appendChild(para)
-    //         i--;
-    //     }
-    // }
-
-    // 
-
-
-        // start timer
-
-     // console.log("this is the timer" , timer)
-
+// Allow player to check their answer using a button
 onClick(btn.checkPlayerAns)
-onClick(btn.playAgain) 
-    // same as play button, ie. generateNums, startTimer, flip card
 
+    // Validating player answers. Move to appropriate location once you've got code right
+
+    compare = (x, y) => playerInput.filter(i => gameNumbers.includes(i)).length;
+        return(compare(playerInput, gameNumbers));
+    // display conditional message to tell player how they did
+    if (compare(playerInput, gameNumbers) < 3) {
+        print("I bet you leave your keys in the fridge... " + smirking_emoji);
+    } else if (compare(playerInput, gameNumbers) > 4) {
+        print("You got them all! " + nerd_emoji);
+    } else
+        print("3 or 4 out of 5 aint so bad... Take your Ginko. " + no_face_emoji);
+
+// Allow player to re-start game
+onClick(btn.playAgain) 
+  // does the same at the Start button
