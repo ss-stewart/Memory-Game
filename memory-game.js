0640d35e9b22ff2d2cc688ad64b4d3d5a316569c
ss-stewart <ss_stewart92@yahoo.com>
2023-04-26 20:12:16 -0400

    added assets folder

--- memory-game.js
+++ memory-game.js
@@ -1,25 +1,30 @@
 // games starts when btn.play is clicked
-onClick(btn.play)
+init ()
+function init () {
 
-    const frontOfCard = document.querySelector('.frontOfCard')
+}
+
+    const frontOfCard = document.getElementsByClassName("frontOfCard")
+    const card = document.createElement('p')
     const timer = document.querySelector('#timer')
     function generateNums () {
         frontOfCard.innerHTML = ''
         // Create an empty array for 5 items
         var fiveRandInt = []
         // Start a loop and run it 5 times
+        
         for(i = 0; i < 5; i++) {
-            const card = document.createElement(p)
             fiveRandInt[i] = Math.floor( Math.random() * 999 )
-            card.textContent = fiveRandInt
+            //card.textContent = fiveRandInt
+            //frontOfCard.appendChild(card)
             }
-            frontOfCard.appendChild(card)
             console.log(fiveRandInt)
     }
   
     // Use the index of the loop to assign the random number to that index of the array
 
         // flip card
+
     function startTimer () {
         timer.innerHTML = ''
 
@@ -44,5 +49,5 @@ onClick(btn.play)
 
 onClick(btn.checkPlayerAns)
 onClick(btn.playAgain) 
-    // same as play button
+    // same as play button, ie. generateNums, startTimer, flip card
 
